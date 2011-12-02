298e77bd021820a60ffdf0b1b066ec9aac7b2df7
Dral <m.c.dral@gmail.com>
2011-12-02 16:57:57 +0100

    makes chatwrapping work with the latest craftbukkit build :D

--- src/com/dral/Chatter/BetterChatWrapper.java
+++ src/com/dral/Chatter/BetterChatWrapper.java
@@ -1,8 +1,7 @@
 package com.dral.Chatter;
 
 public class BetterChatWrapper {
-	
-    private static final int[] characterWidths = new int[] {
+private static final int[] characterWidths = new int[] {
         1, 9, 9, 8, 8, 8, 8, 7, 9, 8, 9, 9, 8, 9, 9, 9,
         8, 8, 8, 8, 9, 9, 8, 9, 8, 8, 8, 8, 8, 9, 9, 9,
         4, 2, 5, 6, 6, 6, 6, 3, 5, 5, 5, 6, 2, 6, 2, 6,
@@ -20,12 +19,12 @@ public class BetterChatWrapper {
         8, 7, 7, 8, 7, 8, 8, 8, 7, 8, 8, 7, 9, 9, 6, 7,
         7, 7, 7, 7, 9, 6, 7, 8, 7, 6, 6, 9, 7, 6, 7, 1
     };
+    private static final String allowedChars = net.minecraft.server.SharedConstants.allowedCharacters;
     private static final char COLOR_CHAR = '\u00A7';
     private static final char SPACE_CHAR = '\u0020';
     private static final char OTHER_COLOR_CHAR = '\u0026';
     private static final int CHAT_WINDOW_WIDTH = 320;
     private static final int CHAT_STRING_LENGTH = 119;
-    private static final String allowedChars = net.minecraft.server.FontAllowedCharacters.allowedCharacters;
 
     public static String[] wrapText(final String text) {
         final StringBuilder out = new StringBuilder();
